eb9aac025fdd23e36dec25cd253e02f5bfdb69e3
Bilal-Ghauri <66202403+Bilal-Ghauri@users.noreply.github.com>
2021-05-06 08:57:35 -0700

    updatedslectedcount()
    created the function if changing the movie it will automatically changes the price of movie

---
+++ Project2/script.js
@@ -0,0 +1,30 @@
+const container = document.querySelector('.seat-container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+let count = document.getElementById('count');
+let total = document.getElementById('total');
+const movie = document.getElementById('movie');
+
+let ticketPrice = +movie.value;
+
+function upDataSelectedCount(){
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const selectedSeatsCount = selectedSeats.length;
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount*ticketPrice;
+
+}
+
+
+container.addEventListener('click', function(e){
+    e.preventDefault();
+    if(e.target.classList.contains('seat') && 
+    !e.target.classList.contains('occupied')){
+        e.target.classList.toggle('selected')
+        upDataSelectedCount();
+    }
+})
+
+movie.addEventListener('change', e=>{
+    ticketPrice = +e.target.value;
+    upDataSelectedCount();
+})
