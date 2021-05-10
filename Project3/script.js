d2c3d242d07f9b194e62b52a14e3288defd156ca
Bilal-Ghauri <66202403+Bilal-Ghauri@users.noreply.github.com>
2021-05-10 03:55:00 -0700

    added timstmp progess
    added  function for playing stop and changing video progress in changing the progress bar

--- Project3/script.js
+++ Project3/script.js
@@ -2,8 +2,27 @@ const video = document.getElementById('video');
 const play = document.getElementById('play');
 const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
-const timestamp = document.getElementById('timestamp');
+let timeStamp = document.getElementById('timestamp');
 
+
+function updateProgress(){
+    progress.value = (video.currentTime / video.duration) * 100;
+}
+
+function updateTimeProgress(){
+    video.currentTime = (progress.value * video.duration) / 100;
+
+    let mins = Math.floor(video.currentTime / 60);
+    if(mins < 10){
+        mins = '0' + String(mins);
+    }
+    let secs = Math.floor(video.currentTime % 60);
+    if(secs < 10){
+        secs = '0' + String(secs);
+    }
+    timeStamp.innerHTML = `${mins}:${secs}`;
+}
+//function for toggle on video click for play and click for pause
 function toggleUpdate(){
     if(video.paused){
         video.play();
@@ -12,7 +31,43 @@ function toggleUpdate(){
     }
 }
 
+//finction for updating icons of play and pause buttons
+function updateIcon(){
+    if(video.paused){
+        play.innerHTML = '<i class="fas fa-play fa-2x"></i>';
+    }else{
+        play.innerHTML = '<i class="fas fa-pause fa-2x"></i>';
+    }
+}
 
+// function for playing video when clicked on  play button 
+function videoPlay(){
+    if(video.paused){
+        video.play();
+    }else{
+        video.pause();
+    }
+}
+
+//finction for stoping video when clicked on stop button and stat video from the beginning after stop.
+function stopVideo(){
+    video.currentTime = 0;
+    video.pause();
+}
+
+
+//eventlistener for pause and play when clicked on screen
 video.addEventListener('click' , toggleUpdate);
+
+//event listener for updating the icon of play button when clicked on screen.
 video.addEventListener('play' , updateIcon);
-video.addEventListener('stop' , updateIcon);
+//finction for pause buttom
+video.addEventListener('pause' , updateIcon);
+//function on play button when clicked the play button video will start.
+play.addEventListener('click' , videoPlay);
+//function on stop button when clicked the stop button video will stop and paused.
+stop.addEventListener('click' , stopVideo)
+//function for updating progress when time updates/
+video.addEventListener('timeupdate' , updateProgress)
+
+progress.addEventListener('change', updateTimeProgress)
