3c1e75328d2ac0773785c6d5900b3a373b095dba
Bilal-Ghauri <66202403+Bilal-Ghauri@users.noreply.github.com>
2021-09-25 21:22:22 +0500

    completed Project 4
    completed project 4 with full functionailites

--- project4/script2.js
+++ project4/script2.js
@@ -1,9 +1,10 @@
 const country = document.getElementById('country')
 const coRates = document.getElementById('coRates')
-
+const heading  = document.getElementById('h2')
 
 function conversion(){
     const countryVal = country.value;
+    heading.innerHTML = `1 ${countryVal} Is Equals to :`
     fetch(`https://v6.exchangerate-api.com/v6/98f86149850e2234a78d914f/latest/${countryVal}`)
     .then(res => res.json())
     .then(data => {
@@ -13,7 +14,7 @@ function conversion(){
             let child = document.createElement('h4')
             for(let i=0 ; i<country.length ; i++){
                 if(country[i].value === key){
-                    child.innerHTML = `${country[i].innerHTML} : ${currencyRate[key]}`
+                    child.innerHTML = `${country[i].innerHTML} : ${currencyRate[key]} ${key}`
                 }
             }
             coRates.appendChild(child)
